b5406f43bbc1f25de642a6378bccc3eb3134d7c9
jRimbault <jacques.rimbault@gmail.com>
2024-04-22 10:27:09 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,7 +11,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+local lazy = require("lazy")
+lazy.setup({
     "joshdick/onedark.vim",
     "fxn/vim-monochrome",
 }, opts)
