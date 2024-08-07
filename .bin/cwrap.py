e2d31822aebae9b5f33fe5595437cb40c2fa5593
jRimbault <jacques.rimbault@gmail.com>
2024-08-07 11:05:37 +0200

    wrapper script

--- .bin/cwrap.py
+++ .bin/cwrap.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Run a command and display its output in a scrolling window.
+Doesn't particularly work well with outputs with lots of control characters.
+"""
 
 import argparse
 import shutil
@@ -7,6 +11,7 @@ import sys
 import time
 from contextlib import contextmanager
 from dataclasses import dataclass
+from datetime import datetime
 from queue import Empty, Queue
 from threading import Thread
 from typing import IO, TextIO, Tuple
@@ -35,11 +40,11 @@ def main(args: Opt):
         consumer(proc.stdout, child_lines, sys.stdout),
         consumer(proc.stderr, child_lines, sys.stdout),
     ):
-        start_time = time.time()
+        start_time = datetime.now()
         output_lines: list[Tuple[str, TextIO]] = []
 
         while True:
-            elapsed = format_time(int(time.time() - start_time))
+            elapsed = datetime.now() - start_time
 
             while True:
                 try:
@@ -47,25 +52,28 @@ def main(args: Opt):
                 except Empty:
                     break
 
-            print(f"\033[H\033[JElapsed time: {elapsed}")
+            print(f"\033[H\033[JElapsed time {elapsed}")
             print(HR)
             for line, source in output_lines[-args.num_lines :]:
                 print(line, end="", file=source)
             output_lines = output_lines[-args.num_lines :]
 
             if proc.poll() is not None and child_lines.empty():
+                # returncode has been set
+                # child_lines has been consumed
                 break
 
-            time.sleep(0.01)
+            time.sleep(0.1)
 
-        elapsed = format_time(int(time.time() - start_time))
+        elapsed = datetime.now() - start_time
         print(HR)
-        print(f"Finished in: {elapsed}")
+        print(f"Finished in {elapsed}")
         return proc.returncode
 
 
 @contextmanager
 def consumer(reader: IO[str] | None, queue: Queue[Tuple[str, TextIO]], source: TextIO):
+    """Consume a reader in a child thread and sends its output to a queue"""
     if reader is None:
         raise Exception("no IO to read")
     child = Thread(target=enqueue_output, args=(reader, queue, source))
@@ -77,29 +85,15 @@ def consumer(reader: IO[str] | None, queue: Queue[Tuple[str, TextIO]], source: T
 
 
 def enqueue_output(reader: IO[str], queue: Queue[Tuple[str, TextIO]], source: TextIO):
+    """Consume reader and puts its output on the queue"""
     for line in iter(reader.readline, ""):
         queue.put((line, source))
     reader.close()
 
 
-def format_time(seconds: int):
-    h, rem = divmod(seconds, 3600)
-    m, s = divmod(rem, 60)
-    if h > 0:
-        return f"{int(h)}h {int(m):02}m {int(s):02}s"
-    elif m > 0:
-        return f"{int(m)}m {int(s):02}s"
-    else:
-        return f"{int(s)}s"
-
-
 def parse_args(argv: list[str]):
-    parser = argparse.ArgumentParser(
-        description="""
-        Run a command and display its output in a scrolling window.
-        Doesn't particularly work well with outputs with lots of control characters.
-        """
-    )
+    """Parse arguments given to the script"""
+    parser = argparse.ArgumentParser(description=__doc__)
     term = shutil.get_terminal_size((80, 20))
     parser.add_argument("command", help="The command to run")
     parser.add_argument(
