82521d4d54da5a5f16c1a76c4733cdc52c9c13f4
jRimbault <jacques.rimbault@gmail.com>
2024-09-11 16:11:55 +0200

    nerdfont

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,46 +28,13 @@ lazy.setup({
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
     -- nice bar at the bottom
-	{
-		'itchyny/lightline.vim',
-		lazy = false, -- also load at start since it's UI
-		config = function()
-			-- no need to also show mode in cmd line when we have bar
-			vim.o.showmode = false
-			vim.g.lightline = {
-				active = {
-					left = {
-						{ 'mode', 'paste' },
-						{ 'readonly', 'filename', 'modified' }
-					},
-					right = {
-						{ 'lineinfo' },
-						{ 'percent' },
-						{ 'fileencoding', 'filetype' }
-					},
-				},
-				component_function = {
-					filename = 'LightlineFilename'
-				},
-			}
-			function LightlineFilenameInLua(opts)
-				if vim.fn.expand('%:t') == '' then
-					return '[No Name]'
-				else
-					return vim.fn.getreg('%')
-				end
-			end
-			-- https://github.com/itchyny/lightline.vim/issues/657
-			vim.api.nvim_exec(
-				[[
-				function! g:LightlineFilename()
-					return v:lua.LightlineFilenameInLua()
-				endfunction
-				]],
-				true
-			)
-		end
-	},
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup()
+        end
+    },
     -- LSP
     {
 		'neovim/nvim-lspconfig',
