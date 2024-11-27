13af9c2b8369a3855bc4ab3b682f1fb2c94b8dc6
jRimbault <jacques.rimbault@gmail.com>
2024-11-27 10:36:08 +0100

    update

--- .bin/get-hosts.py
+++ .bin/get-hosts.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
 from pathlib import Path
 
-with open(Path.home() / ".ssh/config", encoding="utf8") as f:
+
+file = sys.argv[1] if len(sys.argv) > 1 else Path.home() / ".ssh/config"
+
+with open(file, encoding="utf8") as f:
     hosts = {
         host.strip()
         for line in f
@@ -11,4 +15,5 @@ with open(Path.home() / ".ssh/config", encoding="utf8") as f:
         if "*" not in host
     }
 
+hosts = sorted(hosts)
 print(*hosts, sep="\n")
