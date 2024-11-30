e7fa8806cc3eb475ea4da78aaa8f088f821522cb
jRimbault <jacques.rimbault@gmail.com>
2024-11-30 23:46:19 +0100

    show my hash utility

---
+++ .bin/hash.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# pyright: strict
+
+import argparse
+import hashlib
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Options:
+    file: Path
+    algorithm: str
+
+
+def main(options: Options) -> None:
+    with options.file.open("rb") as f:
+        hash_value = hashlib.file_digest(f, options.algorithm).hexdigest()
+    print(f"{hash_value} {options.file.absolute()}")
+
+
+def parse_args(argv: list[str]) -> Options:
+    parser = argparse.ArgumentParser(description="File hashing utility.")
+    parser.add_argument(
+        "algorithm",
+        choices=sorted(hashlib.algorithms_guaranteed),
+        help="Hashing algorithm to use (guaranteed to be available).",
+    )
+    parser.add_argument("file", type=Path, help="Path to the file to hash.")
+    args = parser.parse_args(argv)
+    return Options(file=args.file, algorithm=args.algorithm)
+
+
+if __name__ == "__main__":
+    sys.exit(main(parse_args(sys.argv[1:])))
