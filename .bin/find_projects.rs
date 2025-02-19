481fdf17681447855d51860bc37d54a607db246b
jRimbault <jacques.rimbault@gmail.com>
2025-02-19 10:03:46 +0100

    add faster find project script

---
+++ .bin/find_projects.rs
@@ -0,0 +1,54 @@
+//! This script searches for projects with `.git` or `.hg` directories starting
+//! from a given base directory. If no base directory is provided, it starts
+//! from the current directory.
+//!
+//! # Compilation
+//! To compile this script with optimizations enabled:
+//! ```sh
+//! rustc -O find_projects.rs
+//! ```
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+use std::sync::mpsc;
+use std::thread;
+
+fn search_projects(base: &Path, sender: mpsc::Sender<PathBuf>) {
+    let mut stack = Vec::from([base.to_path_buf()]);
+
+    while let Some(path) = stack.pop() {
+        if let Ok(entries) = fs::read_dir(&path) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if path.is_dir() {
+                        if path.join(".git").exists() || path.join(".hg").exists() {
+                            sender.send(path).unwrap();
+                        } else {
+                            stack.push(path);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let base = if args.len() > 1 {
+        PathBuf::from(&args[1])
+    } else {
+        env::current_dir().expect("Failed to get current directory")
+    };
+
+    thread::scope(|s| {
+        let (sender, receiver) = mpsc::channel();
+        s.spawn(|| search_projects(&base, sender));
+
+        for project in receiver {
+            println!("{}", project.strip_prefix(&base).unwrap().display());
+        }
+    });
+}
